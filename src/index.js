f4223936b41ecaec00f6e73af803d973337d181d
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-06 11:28:09 -0500

    removing unnecessary code lines

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import analizeFile from './utils.js';
 import _ from 'lodash';
 
 export default function gendiff(filepath1, filepath2) {
-  const curretDirectory = process.cwd();
-  const pathFileOne = path.resolve(curretDirectory, filepath1);
-  const pathFileTwo = path.resolve(curretDirectory, filepath2);
+  const pathFileOne = path.resolve(process.cwd(), filepath1);
+  const pathFileTwo = path.resolve(process.cwd(), filepath2);
   const fileOneAnalysis = analizeFile(pathFileOne);
   const fileTwoAnalysis = analizeFile(pathFileTwo);
   if (fileOneAnalysis && fileTwoAnalysis) {
     const keysFileOne = Object.keys(fileOneAnalysis);
     const keysFileTwo = Object.keys(fileTwoAnalysis);
     const unionKeys = _.union(keysFileOne, keysFileTwo).sort();
-    console.log('Keys File1: ', keysFileOne);
-    console.log('Keys File2: ', keysFileTwo);
-    console.log('Union: ', unionKeys);
     const result = unionKeys.reduce((acc, key) => {
       const key1 = '-' + key;
       const key2 = '+' + key;
