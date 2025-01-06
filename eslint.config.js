3b4b0f9c300e17b92a30d7c7a51e9b09a56ee06a
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-06 16:16:33 -0500

    fixing imports from eslint.config.js file

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from './node_modules/@eslint/js';
+import tseslint from './node_modules/typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
