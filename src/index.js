59c9e77752cba0b9f2cf9fdc93fd59f18a50a545
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-06 11:24:15 -0500

    removing smells from index.js file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import analizeFile from './utils.js';
 import _ from 'lodash';
 
 export default function gendiff(filepath1, filepath2) {
-  const pathFileOne = path.resolve(process.cwd(), filepath1);
-  const pathFileTwo = path.resolve(process.cwd(), filepath2);
+  const curretDirectory = process.cwd();
+  const pathFileOne = path.resolve(curretDirectory, filepath1);
+  const pathFileTwo = path.resolve(curretDirectory, filepath2);
   const fileOneAnalysis = analizeFile(pathFileOne);
   const fileTwoAnalysis = analizeFile(pathFileTwo);
   if (fileOneAnalysis && fileTwoAnalysis) {
