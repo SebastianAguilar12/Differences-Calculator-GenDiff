5e96cd71d9b7d4c52128b2fad8818400fd53c22f
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-07 23:09:56 -0500

    updating eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,15 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
