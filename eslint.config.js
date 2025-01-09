157b6130f9b95c79284e22109771c931c181997d
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-08 23:41:50 -0500

    setting up eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,60 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 
+// mimic CommonJS variables -- not needed if using CommonJS
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
   {
-    env: {
-      browser: true,
-      node: true,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     },
-    globals: {
-      ...globals.node,
-      ...globals.jest,
+    plugins: { import: importPlugin },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-amd': 'off',
+      'import/no-mutable-exports': 'off',
+      'import/newline-after-import': 'off',
+      'import/first': 'off',
+    },
+  },
 ];
