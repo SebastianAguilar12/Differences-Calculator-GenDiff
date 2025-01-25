d4803164ea25d58df7f0a73ee2daffa7c1d891ef
SebastianAguilar12 <sebas09981@gmail.com>
2025-01-25 15:22:00 -0500

    updating project with indent files and tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,7 @@
 import path from 'node:path';
+import { describe } from 'node:test';
 import gendiff from '../src/index.js';
+import * as functions from '../src/utils.js';
 
 test('gendiff with two .json extension files', () => {
   const file1 = path.join(process.cwd(), '__fixtures__', 'file1.json');
@@ -25,6 +27,124 @@ test('gendiff with two .json extension files', () => {
     '- nombre': 'Sebastian',
     '+ nombre': 'María Fernanda',
   };
-  expect(comparision1).toBe(JSON.stringify(expectedObject1, null, 2));
-  expect(comparision2).toBe(JSON.stringify(expectedObject2, null, 2));
+  expect(comparision1).toBe(functions.stylish(expectedObject1));
+  expect(comparision2).toBe(functions.stylish(expectedObject2));
+});
+
+test('gendiff with two .yml extension files', () => {
+  const ymlFile1 = path.join(process.cwd(), 'parsers', 'ymlfile1.yml');
+  const ymlFile2 = path.join(process.cwd(), 'parsers', 'ymlfile2.yml');
+  const ymlComparision1 = gendiff(ymlFile1, ymlFile2);
+  const expectedYml = {
+    '- follow': false,
+    host: 'codica.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': 50,
+    '+ timeout': 20,
+    '+ verbose': true,
+  };
+  expect(ymlComparision1).toEqual(functions.stylish(expectedYml));
+});
+
+describe('test indent files', () => {
+  test('testing indent .json files', () => {
+    const indentFile1 = path.join(process.cwd(), '__fixtures__', 'indentFile1.json');
+    const indentFile2 = path.join(process.cwd(), '__fixtures__', 'indentFile2.json');
+    const indentFilesDiff = gendiff(indentFile1, indentFile2);
+    const expectedIndent = {
+      common: {
+        '+ follow': false,
+        setting1: 'Value 1',
+        '- setting2': 200,
+        '- setting3': true,
+        '+ setting3': null,
+        '+ setting4': 'blah blah',
+        '+ setting5': {
+          key5: 'value5',
+        },
+        setting6: {
+          doge: {
+            '- wow': '',
+            '+ wow': 'so much',
+          },
+          key: 'value',
+          '+ ops': 'vops',
+        },
+      },
+      group1: {
+        '- baz': 'bas',
+        '+ baz': 'bars',
+        foo: 'bar',
+        '- nest': {
+          key: 'value',
+        },
+        '+ nest': 'str',
+      },
+      '- group2': {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+      '+ group3': {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    };
+    expect(indentFilesDiff).toEqual(functions.stylish(expectedIndent));
+  });
+  test('testing indent .yml files', () => {
+    const indentYmlFile1 = path.join(process.cwd(), 'parsers', 'indentFile1.yaml');
+    const indentYmlFile2 = path.join(process.cwd(), 'parsers', 'indentFile2.yaml');
+    const indentYmlDiff = gendiff(indentYmlFile1, indentYmlFile2);
+    const expectedIndent = {
+      common: {
+        '+ follow': false,
+        setting1: 'Value 1',
+        '- setting2': 200,
+        '- setting3': true,
+        '+ setting3': null,
+        '+ setting4': 'blah blah',
+        '+ setting5': {
+          key5: 'value5',
+        },
+        setting6: {
+          doge: {
+            '- wow': '',
+            '+ wow': 'so much',
+          },
+          key: 'value',
+          '+ ops': 'vops',
+        },
+      },
+      group1: {
+        '- baz': 'bas',
+        '+ baz': 'bars',
+        foo: 'bar',
+        '- nest': {
+          key: 'value',
+        },
+        '+ nest': 'str',
+      },
+      '- group2': {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+      '+ group3': {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    };
+    expect(indentYmlDiff).toEqual(functions.stylish(expectedIndent));
+  });
 });
