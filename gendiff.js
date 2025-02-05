69734ddb6aaec63fb2f082e1693384c86fd68527
SebastianAguilar12 <sebas09981@gmail.com>
2025-02-05 11:30:24 -0500

    fixing the error on gendiff code

--- gendiff.js
+++ gendiff.js
@@ -6,26 +6,30 @@ import stylish from './src/formatters/stylish.js';
 import json from './src/formatters/index.js';
 import plain from './src/formatters/plain.js';
 
-const program = new Command();
+if (import.meta.url === `file://${process.argv[1]}`) {
+  const program = new Command();
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .version('0.0.1')
-  .option('-f, --format [type]', 'output format (default: "stylish")')
-  .action((filepath1, filepath2, options) => {
-    const { format } = options;
-    const formatName = ((formatType) => {
-      if (formatType === 'json') {
-        return json;
-      }
-      if (formatType === 'plain') {
-        return plain;
-      }
-      return stylish;
-    })(format);
-    console.log(gendiff(filepath1, filepath2, formatName));
-  });
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .version('0.0.1')
+    .option('-f, --format [type]', 'output format (default: "stylish")')
+    .action((filepath1, filepath2, options) => {
+      const { format } = options;
+      const formatName = ((formatType) => {
+        if (formatType === 'json') {
+          return json;
+        }
+        if (formatType === 'plain') {
+          return plain;
+        }
+        return stylish;
+      })(format);
+      console.log(gendiff(filepath1, filepath2, formatName));
+    });
 
-program.parse();
+  program.parse();
+}
+
+export default gendiff;
