19956bc03f4dae03e729302d316f882f005207e3
SebastianAguilar12 <sebas09981@gmail.com>
2025-02-05 11:42:15 -0500

    recalculating number of nested spaces on stylish function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-export default function stylish(value, reemplazador = ' ', spaces = 3, isMainObject = true, depthLevel = 1) {
+export default function stylish(value, reemplazador = ' ', spaces = 2, isMainObject = true, depthLevel = 1) {
   const keysArray = Object.keys(value);
   if (value === null) {
     return 'null';
@@ -30,7 +30,7 @@ export default function stylish(value, reemplazador = ' ', spaces = 3, isMainObj
         ? `{\n${keysArray.reduce((acc, key, index) => {
           const curr = value[key];
           const isOb = _.isObject(curr);
-          const currVal = isOb ? stylish(curr, reemplazador, spaces, false, depthLevel + 1) : curr;
+          const currVal = isOb ? stylish(curr, reemplazador, spaces, false, depthLevel + 2) : curr;
           const indent = `${reemplazador.repeat(spaces * depthLevel)}`;
           const isDifference = key.startsWith('+') || key.startsWith('-');
           const separador = isDifference ? `${indent}${key}: ` : `${indent}  ${key}: `;
@@ -39,7 +39,7 @@ export default function stylish(value, reemplazador = ' ', spaces = 3, isMainObj
         : `{\n${keysArray.reduce((acc, key, index) => {
           const curr = value[key];
           const isOb = _.isObject(curr);
-          const currVal = isOb ? stylish(curr, reemplazador, spaces, false, depthLevel + 1) : curr;
+          const currVal = isOb ? stylish(curr, reemplazador, spaces, false, depthLevel + 2) : curr;
           const indent = `${reemplazador.repeat(spaces * depthLevel)}`;
           const isDifference = key.startsWith('+') || key.startsWith('-');
           const separador = isDifference ? `${indent}${key}: ` : `${indent}  ${key}: `;
