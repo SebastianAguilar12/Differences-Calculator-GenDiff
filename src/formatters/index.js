9f73d52db2c525099e2676808a78d137d96f8aa6
SebastianAguilar12 <sebas09981@gmail.com>
2025-02-10 22:44:39 -0500

    adjusting json format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,16 @@
+import _ from 'lodash';
+
 export default function json(object) {
-  const children = object.children || [];
-  const type = object.type || '';
-  return { children, type };
+  const { children, type } = object;
+  if (children) {
+    children.forEach((child, index) => {
+      if (typeof child === 'string') {
+        children[index] = { type: 'text', value: child };
+      } else if (_.isObject(child) && Array.isArray(child)) {
+        children[index] = json(child);
+      }
+    });
+    return { children, type };
+  }
+  return { type };
 }
